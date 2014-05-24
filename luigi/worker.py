soocodes/luigi

commit 31ff5278cb2ad3ab2d35c9907278165825d0f927

include worker id and host in log output
Makes log output more usable for troubleshooting when somewhere a worker process is hung. The "is currently run by worker-RANDOM_NUMBER" error message will now include the host, and logs on that host will let associate it with the pid.


Former-commit-id: c75085b3c812c8054ef76d39676ab473e45b9600

luigi/worker.py
@@ -83,7 +83,7 @@ class Worker(object):
             worker_processes = 1
 
         self.worker_processes = worker_processes
-        self.host = socket.gethostname()
+        self.host = socket.gethostbyaddr(socket.gethostname())[0]
         self._scheduled_tasks = {}
 
         # store the previous tasks executed by the same worker
@@ -103,7 +103,7 @@ class Worker(object):
                 while True:
                     self._should_stop.wait(ping_interval)
                     if self._should_stop.is_set():
-                        logger.info("Worker was stopped. Shutting down Keep-Alive thread")
+                        logger.info("Worker %s was stopped. Shutting down Keep-Alive thread" % worker_id)
                         break
                     try:
                         scheduler.ping(worker=worker_id)
@@ -240,7 +240,7 @@ class Worker(object):
     def _run_task(self, task_id):
         task = self._scheduled_tasks[task_id]
 
-        logger.info('[pid %s] Running   %s', os.getpid(), task_id)
+        logger.info('[pid %s] Worker %s running   %s', os.getpid(), self._id, task_id)
         try:
             # Verify that all the tasks are fulfilled!
             ok = True
@@ -254,7 +254,7 @@ class Worker(object):
                 raise RuntimeError('Unfulfilled dependency %r at run time!\nPrevious tasks: %r' % (missing_dep.task_id, self._previous_tasks))
             task.run()
             error_message = json.dumps(task.on_success())
-            logger.info('[pid %s] Done      %s', os.getpid(), task_id)
+            logger.info('[pid %s] Worker %s done      %s', os.getpid(), self._id, task_id)
             task.trigger_event(Event.SUCCESS, task)
             status = DONE
 
@@ -262,7 +262,7 @@ class Worker(object):
             raise
         except Exception as ex:
             status = FAILED
-            logger.exception("[pid %s] Error while running %s", os.getpid(), task)
+            logger.exception("[pid %s] Worker %s failed    %s", os.getpid(), self._id, task)
             error_message = task.on_failure(ex)
             task.trigger_event(Event.FAILURE, task, ex)
             subject = "Luigi: %s FAILED" % task

luigi/scheduler.py
@@ -12,6 +12,7 @@
 # License for the specific language governing permissions and limitations under
 # the License.
 
+import datetime
 import os
 import logging
 import time
@@ -42,8 +43,8 @@ STATUS_TO_UPSTREAM_MAP = {FAILED: UPSTREAM_FAILED, RUNNING: UPSTREAM_RUNNING, PE
 
 class Task(object):
     def __init__(self, status, deps):
-        self.stakeholders = set()  # workers that are somehow related to this task (i.e. don't prune while any of these workers are still active)
-        self.workers = set()  # workers that can perform task - task is 'BROKEN' if none of these workers are active
+        self.stakeholders = set()  # workers ids that are somehow related to this task (i.e. don't prune while any of these workers are still active)
+        self.workers = set()  # workers ids that can perform task - task is 'BROKEN' if none of these workers are active
         if deps is None:
             self.deps = set()
         else:
@@ -52,11 +53,22 @@ class Task(object):
         self.time = time.time()  # Timestamp when task was first added
         self.retry = None
         self.remove = None
-        self.worker_running = None  # the worker that is currently running the task or None
+        self.worker_running = None  # the worker id that is currently running the task or None
         self.expl = None
 
     def __repr__(self):
         return "Task(%r)" % vars(self)
+
+
+class Worker(object):
+    """ Structure for tracking worker activity and keeping their references """
+    def __init__(self, id):
+        self.id = id
+        self.reference = None  # reference to the worker in the real world. (Currently a dict containing just the host)
+        self.last_active = None  # seconds since epoch
+
+    def __str__(self):
+        return "%s on %s, last active %s" % (self.id, self.reference, datetime.datetime.utcfromtimestamp(self.last_active).isoformat())
 
 
 class CentralPlannerScheduler(Scheduler):
@@ -76,13 +88,12 @@ class CentralPlannerScheduler(Scheduler):
         worker_disconnect_delay -- If a worker hasn't communicated for this long, remove it from active workers
         '''
         self._state_path = state_path
-        self._tasks = {}
+        self._tasks = {}  # map from id to a Task object
         self._retry_delay = retry_delay
         self._remove_delay = remove_delay
         self._worker_disconnect_delay = worker_disconnect_delay
-        self._active_workers = {}  # map from id to timestamp (last updated)
+        self._active_workers = {}  # map from id to a Worker object
         self._task_history = task_history or history.NopHistory()
-        # TODO: have a Worker object instead, add more data to it
 
     def dump(self):
         state = (self._tasks, self._active_workers)
@@ -94,6 +105,7 @@ class CentralPlannerScheduler(Scheduler):
         else:
             logger.info("Saved state in %s", self._state_path)
 
+    # prone to lead to crashes when old state is unpickled with updated code. TODO some kind of version control?
     def load(self):
         if os.path.exists(self._state_path):
             logger.info("Attempting to load state from %s", self._state_path)
@@ -107,10 +119,10 @@ class CentralPlannerScheduler(Scheduler):
         logger.info("Starting pruning of task graph")
         # Delete workers that haven't said anything for a while (probably killed)
         delete_workers = []
-        for worker in self._active_workers:
-            if self._active_workers[worker] < time.time() - self._worker_disconnect_delay:
-                logger.info("worker %r updated at %s timed out (no contact for >=%ss)", worker, self._active_workers[worker], self._worker_disconnect_delay)
-                delete_workers.append(worker)
+        for worker in self._active_workers.values():
+            if worker.last_active < time.time() - self._worker_disconnect_delay:
+                logger.info("Worker %s timed out (no contact for >=%ss)", worker, self._worker_disconnect_delay)
+                delete_workers.append(worker.id)
 
         for worker in delete_workers:
             self._active_workers.pop(worker)
@@ -147,10 +159,12 @@ class CentralPlannerScheduler(Scheduler):
                 task.status = PENDING
         logger.info("Done pruning task graph")
 
-    def update(self, worker):
-        # update timestamp so that we keep track
-        # of whenever the worker was last active
-        self._active_workers[worker] = time.time()
+    def update(self, worker_id, worker_reference=None):
+        """ Keep track of whenever the worker was last active """
+        worker = self._active_workers.setdefault(worker_id, Worker(worker_id))
+        if worker_reference:
+            worker.reference = worker_reference
+        worker.last_active = time.time()
 
     def add_task(self, worker, task_id, status=PENDING, runnable=True, deps=None, expl=None):
         """
@@ -193,7 +207,7 @@ class CentralPlannerScheduler(Scheduler):
         # nothing it can wait for
 
         # Return remaining tasks that have no FAILED descendents
-        self.update(worker)
+        self.update(worker, {'host': host})
         best_t = float('inf')
         best_task = None
         locally_pending_tasks = 0
@@ -204,7 +218,7 @@ class CentralPlannerScheduler(Scheduler):
                 continue
 
             if task.status == RUNNING:
-                running_tasks.append({'task_id': task_id, 'worker': task.worker_running})
+                running_tasks.append({'task_id': task_id, 'worker': str(self._active_workers.get(task.worker_running))})
 
             if task.status != PENDING:
                 continue

test/central_planner_test.py
@@ -155,7 +155,7 @@ class CentralPlannerTest(unittest.TestCase):
         self.assertEqual(r['task_id'], None)  # Worker Y is pending on A to be done
         s = r['running_tasks'][0]
         self.assertEqual(s['task_id'], 'A')
-        self.assertEqual(s['worker'], 'X')
+        self.assert_(s['worker'].startswith('X on '))
 
 class TestParameterSplit(unittest.TestCase):
     task_id_examples = [

commit d9bb836b3eb6b424169f3e35507b09da74c8dbb3

Hotfixed an error caused by 31ff5278cb2ad3ab2d35c9907278165825d0f927 [formerly c75085b3c812c8054ef76d39676ab473e45b9600]
Former-commit-id: b5700bd5e3a7f3680f10eb2af029ea3e6fdfc3dc

luigi/worker.py
@@ -83,7 +83,7 @@ class Worker(object):
             worker_processes = 1
 
         self.worker_processes = worker_processes
-        self.host = socket.gethostbyaddr(socket.gethostname())[0]
+        self.host = socket.gethostname()
         self._scheduled_tasks = {}
 
         # store the previous tasks executed by the same worker